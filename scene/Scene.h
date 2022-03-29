218d8f101321378ce0b2b8de90492d978391f3b3
Eonsinde <olasinde.eon@gmail.com>
2022-03-29 20:12:02 +0100

    Created Scene Interface

--- scene/Scene.h
+++ scene/Scene.h
@@ -2,11 +2,21 @@
 #define __hSCENE_H__
 
 namespace Hound {
+	class Application;
+
 	class Scene {
+	protected:
+		friend class Application;
 	public:
 		Scene() = default;
-		~Scene() = default;
+		virtual ~Scene() = default;
 
+		virtual void Init() = 0;
+		virtual void LoadScene() = 0;
+		virtual void UnloadScene() = 0;
+
+		virtual void Update(float deltaTime) = 0;
+		virtual void Draw() = 0;
 	};
 }
 
