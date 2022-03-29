d244f10748d20950241d114e113c9551da3a5a20
Eonsinde <olasinde.eon@gmail.com>
2022-03-29 19:44:45 +0100

    Fixed Linking Issues

---
+++ core/Application.h
@@ -0,0 +1,13 @@
+#ifndef __hAPPLICATION_H__
+#define __hAPPLICATION_H__
+
+#include <iostream>
+
+namespace Hound {
+	class Application {
+	public:
+		
+	};
+}
+
+#endif
---
+++ scene/Scene.h
@@ -0,0 +1,13 @@
+#ifndef __hSCENE_H__
+#define __hSCENE_H__
+
+namespace Hound {
+	class Scene {
+	public:
+		Scene() = default;
+		~Scene() = default;
+
+	};
+}
+
+#endif
