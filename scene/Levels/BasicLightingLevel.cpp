e041f58aa9b435ae2dd0b9aa0d1b40b7576d9ad1
Eonsinde <olasinde.eon@gmail.com>
2022-04-03 22:53:24 +0100

    Creating lighting in the scene

---
+++ scene/Levels/BasicLightingLevel.cpp
@@ -0,0 +1,115 @@
+#include "BasicLightingLevel.h"
+
+#include "../../shaders/Shader.h"
+#include "../../core/Input.h"
+
+
+void BasicLightingLevel::Init()
+{
+    float vertices[] = {
+      -0.5f, -0.5f, -0.5f,
+       0.5f, -0.5f, -0.5f,
+       0.5f,  0.5f, -0.5f,
+       0.5f,  0.5f, -0.5f,
+      -0.5f,  0.5f, -0.5f,
+      -0.5f, -0.5f, -0.5f,
+
+      -0.5f, -0.5f,  0.5f,
+       0.5f, -0.5f,  0.5f,
+       0.5f,  0.5f,  0.5f,
+       0.5f,  0.5f,  0.5f,
+      -0.5f,  0.5f,  0.5f,
+      -0.5f, -0.5f,  0.5f,
+
+      -0.5f,  0.5f,  0.5f,
+      -0.5f,  0.5f, -0.5f,
+      -0.5f, -0.5f, -0.5f,
+      -0.5f, -0.5f, -0.5f,
+      -0.5f, -0.5f,  0.5f,
+      -0.5f,  0.5f,  0.5f,
+
+       0.5f,  0.5f,  0.5f,
+       0.5f,  0.5f, -0.5f,
+       0.5f, -0.5f, -0.5f,
+       0.5f, -0.5f, -0.5f,
+       0.5f, -0.5f,  0.5f,
+       0.5f,  0.5f,  0.5f,
+
+      -0.5f, -0.5f, -0.5f,
+       0.5f, -0.5f, -0.5f,
+       0.5f, -0.5f,  0.5f,
+       0.5f, -0.5f,  0.5f,
+      -0.5f, -0.5f,  0.5f,
+      -0.5f, -0.5f, -0.5f,
+
+      -0.5f,  0.5f, -0.5f,
+       0.5f,  0.5f, -0.5f,
+       0.5f,  0.5f,  0.5f,
+       0.5f,  0.5f,  0.5f,
+      -0.5f,  0.5f,  0.5f,
+      -0.5f,  0.5f, -0.5f,
+    };
+
+    // create VAO and bind buffer
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    mShader = new Shader("./shaders/codes/basic.vert", "./shaders/codes/basic.frag");
+}
+
+void BasicLightingLevel::LoadScene()
+{
+
+}
+
+void BasicLightingLevel::UnloadScene()
+{
+    if (mShader)
+        delete mShader;
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+}
+
+void BasicLightingLevel::Update(float deltaTime)
+{
+    if (Hound::Input::IsKeyPressed(Hound::Key::KPAdd))
+        std::cout << "Adding to movement speed\n";
+    if (Hound::Input::IsKeyPressed(Hound::Key::KPSubtract))
+        std::cout << "Removing from movement speed\n";
+}
+
+void BasicLightingLevel::Draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0784f, 0.0784f, 0.0784f, 1.0);
+    glEnable(GL_DEPTH_TEST);
+
+    // use shader and set uniforms where needed
+    mShader->use();
+
+    mShader->setVec3fv("uPixelColor", glm::value_ptr(glm::vec3(1.0f, 0.5f, 0.31f)));
+
+    model = glm::scale(idMat, glm::vec3(1.0f, 1.0f, 1.0f));
+    // x + Rsin@, y + Rcos@ - formula to get points of a circle
+    //view = glm::translate(idMat, glm::vec3(5.0f * glm::sin(glfwGetTime()), 0.0f, 5.0f * glm::cos(glfwGetTime())));
+    view = mainCamera.GetViewMatrix();
+    //projection = glm::perspective(glm::radians(45.0f), (float)mSceneInfo.width / (float)mSceneInfo.height, 0.1f, 100.0f);
+    projection = glm::perspective(glm::radians(mainCamera.Zoom), (float)mSceneInfo.width / (float)mSceneInfo.height, 0.1f, 100.0f);
+
+    mShader->setMat4fv("uModel", glm::value_ptr(model));
+    mShader->setMat4fv("uView", glm::value_ptr(view));
+    mShader->setMat4fv("uProjection", glm::value_ptr(projection));
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+}
---
+++ scene/Levels/BasicLightingLevel.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "../../core/Application.h"
+#include "../Scene.h"
+#include "../Camera.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader;
+
+static Hound::Camera mainCamera;
+
+class BasicLightingApp : public Hound::Application
+{
+	virtual void onKey(int key, int action)
+	{
+		if (key == GLFW_KEY_W)
+			mainCamera.ProcessKeyboard(FORWARD, Application::GetDeltaTime());
+		if (key == GLFW_KEY_S)
+			mainCamera.ProcessKeyboard(BACKWARD, Application::GetDeltaTime());
+		if (key == GLFW_KEY_A)
+			mainCamera.ProcessKeyboard(LEFT, Application::GetDeltaTime());
+		if (key == GLFW_KEY_D)
+			mainCamera.ProcessKeyboard(RIGHT, Application::GetDeltaTime());
+
+		if (key == GLFW_KEY_KP_ADD)
+			mainCamera.MovementSpeed += 0.2f;
+		if (key == GLFW_KEY_KP_SUBTRACT)
+			mainCamera.MovementSpeed -= 0.2f;
+	}
+
+	virtual void onMouseMove(int x, int y)
+	{
+		float xpos = static_cast<float>(x);
+		float ypos = static_cast<float>(y);
+
+		if (firstMouse)
+		{
+			// update with current mouse position
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+		lastX = xpos;
+		lastY = ypos;
+
+		mainCamera.ProcessMouseMovement(xoffset, yoffset);
+	}
+
+protected:
+	float lastX = (float)mInfo.windowWidth / 2.0f;
+	float lastY = (float)mInfo.windowHeight / 2.0f;
+	bool firstMouse = true;
+};
+
+
+class BasicLightingLevel : public Hound::Scene
+{
+public:
+	BasicLightingLevel()
+		: VAO{}, VBO{}, mShader{ nullptr } {
+		char title[]{ "Cube Level" };
+		strcpy_s(mSceneInfo.title, sizeof(title), "Cube Level");
+		mSceneInfo.width = 800;
+		mSceneInfo.height = 600;
+	}
+
+	virtual ~BasicLightingLevel() {
+
+	}
+
+	virtual void Init() override;
+	virtual void LoadScene() override;
+	virtual void UnloadScene() override;
+
+	virtual void Update(float deltaTime) override;
+	virtual void Draw() override;
+
+protected:
+	unsigned int VAO, VBO;
+	Shader* mShader;
+
+	// matrices
+	glm::mat4 idMat{ 1.0f }; // identity matrix
+	glm::mat4 model, view, projection;
+};
+
+
+DECLARE_MAIN(BasicLightingApp, BasicLightingLevel)
+
+
+
+
