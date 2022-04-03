0b614a2d24ab84051d056661e1053420bebe61f2
Eonsinde <olasinde.eon@gmail.com>
2022-04-03 21:33:02 +0100

    Added Input interface to GLFW

---
+++ core/Input.cpp
@@ -0,0 +1,40 @@
+#include "Input.h"
+#include "Application.h"
+
+
+namespace Hound {
+
+	bool Input::IsKeyPressed(const KeyCode key)
+	{
+		auto& window = Application::GetWindow();
+		auto state = glfwGetKey(window, static_cast<int32_t>(key));
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool Input::IsMouseButtonPressed(const MouseCode button)
+	{
+		auto& window = Application::GetWindow();
+		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
+		return state == GLFW_PRESS;
+	}
+
+	glm::vec2 Input::GetMousePosition()
+	{
+		auto& window = Application::GetWindow();
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		return { (float)xpos, (float)ypos };
+	}
+
+	float Input::GetMouseX()
+	{
+		return GetMousePosition().x;
+	}
+
+	float Input::GetMouseY()
+	{
+		return GetMousePosition().y;
+	}
+
+}
