c9bd5237c977cb24c356be0dd4e32f33207eb100
Eonsinde <olasinde.eon@gmail.com>
2022-04-11 16:12:14 +0100

    Added Directional Light Support

---
+++ scene/Levels/DirectionalLightLevel.cpp
@@ -0,0 +1,163 @@
+#include "DirectionalLightLevel.h"
+
+#include "../../shaders/Shader.h"
+#include "../../core/Input.h"
+
+
+void DirectionalLightLevel::Init()
+{
+    float vertices[] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+    };
+
+    // bind light VAO and bind buffer
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    // bind cube VAO and register VBO data in it by binding the VBO 
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    // bind buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+    cubeShader = new Shader("./shaders/codes/basic.vert", "./shaders/codes/basic.frag");
+    lightShader = new Shader("./shaders/codes/ADS.vert", "./shaders/codes/dirlight_ADS.frag");
+
+    mCubeTexture = new Hound::Texture("./assets/textures/wooden_metallic_container.png");
+    mSpecularMapTexture = new Hound::Texture("./assets/textures/wooden_metallic_specular_container.png");
+
+    mDirectionalLight.direction = glm::vec3(-0.2f, -1.0f, -0.3f);
+    mDirectionalLight.ambient = glm::vec3(0.1f);
+    mDirectionalLight.diffuse = glm::vec3(1.0f);
+    mDirectionalLight.specular = glm::vec3(0.5f);
+
+    mCubeMaterial.shininess = 32.0f;
+}
+
+void DirectionalLightLevel::LoadScene()
+{
+
+}
+
+void DirectionalLightLevel::UnloadScene()
+{
+    if (lightShader)
+        delete lightShader;
+    if (cubeShader)
+        delete cubeShader;
+    if (mCubeTexture)
+        delete mCubeTexture;
+
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &lightVAO);
+    glDeleteVertexArrays(1, &cubeVAO);
+}
+
+void DirectionalLightLevel::Update(float deltaTime)
+{
+    if (Hound::Input::IsKeyPressed(Hound::Key::KPAdd))
+        std::cout << "Adding to movement speed\n";
+    if (Hound::Input::IsKeyPressed(Hound::Key::KPSubtract))
+        std::cout << "Removing from movement speed\n";
+}
+
+void DirectionalLightLevel::Draw()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.047058f, 0.047058f, 0.047058f, 1.0);
+    glEnable(GL_DEPTH_TEST);
+
+    // use light shader to render cubes and set uniforms where needed
+    lightShader->use();
+
+    // set Material properties: set texture units 
+    mCubeTexture->activeTexture(0);
+    mSpecularMapTexture->activeTexture(1);
+    lightShader->setInt("uMaterial.diffuse", 0);
+    lightShader->setInt("uMaterial.specular", 1);
+    lightShader->setFloat("uMaterial.shininess", mCubeMaterial.shininess);
+
+    // camera props 
+    lightShader->setVec3fv("uCameraPos", glm::value_ptr(mainCamera.Position));
+    // light properties
+    lightShader->setVec3fv("uDirectionalLight.direction", glm::value_ptr(mDirectionalLight.direction));
+    lightShader->setVec3fv("uDirectionalLight.ambient", glm::value_ptr(mDirectionalLight.ambient));
+    lightShader->setVec3fv("uDirectionalLight.diffuse", glm::value_ptr(mDirectionalLight.diffuse));
+    lightShader->setVec3fv("uDirectionalLight.specular", glm::value_ptr(mDirectionalLight.specular));
+
+    view = mainCamera.GetViewMatrix();
+    projection = glm::perspective(glm::radians(mainCamera.Zoom), (float)mSceneInfo.width / (float)mSceneInfo.height, 0.1f, 100.0f);
+
+    lightShader->setMat4fv("uView", glm::value_ptr(view));
+    lightShader->setMat4fv("uProjection", glm::value_ptr(projection));
+
+    for (int16_t i{}; i < 10; i++) {
+        model = glm::translate(idMat, mCubePositions[i]);
+        model = glm::rotate(model, static_cast<float>(i * 10), glm::vec3(1.0f, 0.3f, 0.25f));
+
+        lightShader->setMat4fv("uModel", glm::value_ptr(model));
+
+        glBindVertexArray(lightVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+    }
+}
