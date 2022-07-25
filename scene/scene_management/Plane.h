3a923cd078b0651618035377f98e3c2b6b453296
eonsinde <olasinde.eon@gmail.com>
2022-07-25 21:48:20 +0100

    Created plane class

--- scene/scene_management/Plane.h
+++ scene/scene_management/Plane.h
@@ -10,17 +10,36 @@
 #include "../Camera.h"
 
 
+class IPrintable {
+	friend std::ostream& operator<<(std::ostream&, const IPrintable&);
+public:
+	IPrintable() = default;
+	virtual ~IPrintable() = default;
+
+protected:
+	virtual void print(std::ostream&) const = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, const IPrintable& rhsObj) {
+	rhsObj.print(os);
+	return os;
+}
+
+
 // to explain how the SimplePlane class works:
 // a plane is defined by a normal and a point 
 // The buildPlane method helps to construct our arbitrary plane
 // and defines its' distance from the origin(0,0,) 
 // the IsInFront method is then used to see if a certain point 
 // is on the plane or behind it and uses the already computed distance to achieve this
-class SimplePlane {
+class SimplePlane : public IPrintable {
 public:
-	SimplePlane() = default;
+	SimplePlane()
+		: m_normal{ 0.f, 1.f, 0.f }, m_distance{0.0f} {
+	};
+
 	SimplePlane(const glm::vec3& pPoint, const glm::vec3& pNormal)
-		: m_normal{ pNormal }, m_distance{ glm::dot(pPoint, pNormal)} {
+		: m_normal{ pNormal }, m_distance{ glm::dot(pPoint, pNormal) } {
 
 	}
 	~SimplePlane() = default;
@@ -30,13 +49,19 @@ public:
 		m_distance = glm::dot(pPoint, m_normal);
 	}
 
+	bool IsInFront(const glm::vec3& pPoint) const {
+		// if m_normal * pPoint >= m_distance: object is in front else : not in front
+		return glm::dot(m_normal, pPoint) >= m_distance;
+	}
+
 	bool IsInFront(const glm::vec4& pPoint) const {
 		return IsInFront(glm::vec3(pPoint.x, pPoint.y, pPoint.z));
 	}
 
-	bool IsInFront(const glm::vec3& pPoint) const {
-		// if m_normal * pPoint >= m_distance: object is in front else : not in front
-		return glm::dot(m_normal, pPoint) >= m_distance;
+protected:
+	void print(std::ostream& os) const override {
+		os << "Normal: " << this->m_normal.x << " " << this->m_normal.y << " " << this->m_normal.z << '\n';
+		os << "Distance: " << this->m_distance << '\n';
 	}
 
 private:
@@ -104,10 +129,14 @@ struct Frustum
 
 Frustum createFrustumFromCamera(const Hound::Camera& cam, float aspect, float fovY,	float zNear, float zFar)
 {
-	// sample arugments: camObj, (1280/720), 45.0f, 0.1f, 100.0f
+	// sample arugments: camObj, (1280/720), 45.0f, 0.1f, 100.0
 	Frustum     frustum;
 	const float halfVSide = zFar * tanf(fovY * .5f);
 	const float halfWSide = halfVSide * aspect;
+
+	// the reason why we multiple by the cam.Front is becos the cam.Front attr affects what the camera is looking at
+	// as defined by our lookAtMatrix(cam.Position, cam.Position + cam.Front, cam.Up) 
+	// since the target is affected by the cam.Front attr, we have to multiple the nearFace and farFace frustum positions by the cam.Front
 	const glm::vec3 frontMultFar = zFar * cam.Front; // implies front multipled by zFar
 
 	frustum.nearFace = { cam.Position + zNear * cam.Front, cam.Front };
@@ -123,6 +152,12 @@ Frustum createFrustumFromCamera(const Hound::Camera& cam, float aspect, float fo
 
 	return frustum;
 }
+
+
+struct Volume
+{
+	virtual bool isOnFrustum(const Frustum& camFrustum) const = 0;
+};
 
 #endif
 
