20f0e610788f4864733d218b0c9f022592e4c358
eonsinde <olasinde.eon@gmail.com>
2022-07-25 21:56:20 +0100

    Increased default speed

--- scene/Camera.h
+++ scene/Camera.h
@@ -18,7 +18,7 @@ enum Camera_Movement {
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 20.0f;
+const float SPEED = 80.0f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
@@ -28,21 +28,7 @@ namespace Hound {
     class Camera
     {
     public:
-        // camera Attributes
-        glm::vec3 Position;
-        glm::vec3 Front;
-        glm::vec3 Up;
-        glm::vec3 Right;
-        glm::vec3 WorldUp;
-        // euler Angles
-        float Yaw;
-        float Pitch;
-        // camera options
-        float MovementSpeed;
-        float MouseSensitivity;
-        float Zoom;
-
-        // constructor with vectors
+               // constructor with vectors
         Camera(glm::vec3 position=glm::vec3(0.0f, 0.0f, 6.0f), glm::vec3 up=glm::vec3(0.0f, 1.0f, 0.0f), float yaw=YAW, float pitch=PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
         {
             Position = position;
@@ -113,6 +99,24 @@ namespace Hound {
                 Zoom = 45.0f;
         }
 
+    public:
+        // camera Attributes
+        glm::vec3 Position;
+        glm::vec3 Front;
+        glm::vec3 Up;
+        glm::vec3 Right;
+        glm::vec3 WorldUp;
+
+        // euler Angles
+        float Yaw;
+        float Pitch;
+        
+        // camera options
+        float MovementSpeed;
+        float MouseSensitivity;
+        float Zoom;
+
+
     private:
         // calculates the front vector from the Camera's (updated) Euler Angles
         void updateCameraVectors()
@@ -123,6 +127,7 @@ namespace Hound {
             front.y = sin(glm::radians(Pitch));
             front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
             Front = glm::normalize(front);
+            //std::cout << "\n\nCam Front: " << front.x << " " << front.y << " " << front.z << "\n\n\n";
             // also re-calculate the Right and Up vector
             Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
             Up = glm::normalize(glm::cross(Right, Front));
