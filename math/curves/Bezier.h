675ed91c997e32d0aded1b25d7fb15c8e4fc35bb
eonsinde <olasinde.eon@gmail.com>
2022-09-06 22:38:25 +0100

    struct to create bezier curve instance

--- math/curves/Bezier.h
+++ math/curves/Bezier.h
@@ -11,17 +11,3 @@ public:
 };
 
 
-template<typename T>
-inline T Interpolate(Bezier<T>& curve, float t) {
-	T pointA = lerp(curve.P1, curve.C1, t);
-	T pointC = lerp(curve.C1, curve.C2, t);
-	T pointB = lerp(curve.C2, curve.P2, t);
-	
-	// using the points now
-	T pointE = lerp(pointA, pointC, t);
-	T pointF = lerp(pointC, pointB, t);
-	// the point R on our bezier curve
-	T pointR = lerp(pointE, pointF, t);
-
-	return pointR;
-}
