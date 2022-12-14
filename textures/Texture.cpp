9fe22014875e7eda20d776b08d39cbcc74593dd3
eonsinde <olasinde.eon@gmail.com>
2022-12-14 23:54:04 +0100

    implement method to change texture wrapping mode

--- textures/Texture.cpp
+++ textures/Texture.cpp
@@ -42,5 +42,54 @@ namespace Hound {
 	Texture::~Texture() {
 		glDeleteTextures(1, &m_ID);
 	}
+
+	void Texture::setWrappingPropetires(int wrapBehaviour)
+	{
+		this->bind();
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapBehaviour); // set texture wrapping to GL_REPEAT (default wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapBehaviour);
+		this->unbind();
+	}
+
+	unsigned int Texture::TextureFromFile(const char* pFileName, const std::string& directory, bool gamma)
+	{
+		std::string filePath = directory + '/' + std::string(pFileName); // e.g assets/girl_model/textures/diffuse_material.png
+
+		// create a GLtexture based on the texture for the mesh
+		unsigned int textureID;
+		glGenTextures(1, &textureID);
+
+		int width, height, nrComponents;
+		unsigned char* data = stbi_load(filePath.c_str(), &width, &height, &nrComponents, 0);
+		if (data)
+		{
+			GLenum format;
+			if (nrComponents == 1)
+				format = GL_RED;
+			else if (nrComponents == 3)
+				format = GL_RGB;
+			else if (nrComponents == 4)
+				format = GL_RGBA;
+
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Texture failed to load at path: " << filePath << std::endl;
+			stbi_image_free(data);
+		}
+
+		return textureID;
+	}
 }
 
--- textures/Texture.h
+++ textures/Texture.h
@@ -1,6 +1,8 @@
-#pragma once
-#include <glad/glad.h>
+#ifndef __HTEXTURE_H__
+#define __HTEXTURE_H__
 
+#include <glad/glad.h>
+#include <string>
 
 namespace Hound {
 	class Texture
@@ -20,10 +22,15 @@ namespace Hound {
 		void activeTexture(int loc) const { glActiveTexture(GL_TEXTURE0 + loc); bind(); }
 		void bind() const { glBindTexture(GL_TEXTURE_2D, m_ID); };
 		void unbind() const { glBindTexture(GL_TEXTURE_2D, 0); };
+		void setWrappingPropetires(int);
+
+		// used to create GLTexture from a given texture file on hard disk
+		static unsigned int TextureFromFile(const char*, const std::string&, bool = 0);
 
 	private:
 		unsigned int m_ID;
 	};
 }
 
+#endif
 
